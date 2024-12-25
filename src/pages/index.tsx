8b0779004bc7bc1f6a3afa6cdb20cfa9ac405eef
fajritaridala <fadjritaridala@gmail.com>
2024-12-25 20:16:17 +0800

    feat: add PageHead feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button } from "@nextui-org/react";
+import PageHead from "@/components/commons/pageHead";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+      <PageHead title="Home page"/>
       <Button color="primary">Button</Button>
     </main>
   );
